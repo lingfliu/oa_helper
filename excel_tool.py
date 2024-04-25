c17648c51abd9e4a9496e235e243c573b2926a76
lingfliu <lingfeng.liu@163.com>
2024-04-25 23:52:40 +0800

    repo init

---
+++ excel_tool.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import xlrd
+import xlwt
+
+file = '17-20培养目标达成度-直接评价.xlsx'
+
+data = xlrd.open_workbook(file)
+
+for tab in data.sheets():
+    print(tab.name)
+    
+
+
